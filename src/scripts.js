95ea686d9eeb5b2d4ab4c9a2c582ae18a2f2ce55
Jeff Sahim <jeffreysahim@gmail.com>
2023-03-02 17:29:00 -0500

    feat: Add getDate function

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,21 @@ import './css/styles.css';
 import datepicker from 'js-datepicker';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/hotel-logo.png';
+const calendar = document.getElementById("calendar") 
+const dateSubmitButton = document.getElementById("dateSubmit") 
+const picker = datepicker(calendar)
+let selectedDate;
 
-console.log('This is the JavaScript entry file - your code begins here.');
-console.log(datepicker)
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+dateSubmitButton.addEventListener("click", getDate)
+
+
+function getDate(event){
+  event.preventDefault()
+  console.log(picker);
+  selectedDate
+}
+
+
+
